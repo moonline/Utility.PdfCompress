15f01e8851e3129dfec2dcd9ea42d46fb07cf383
Tobias Blaser <tobiasblaser@colvarim.ch>
2017-03-24 16:22:06 -0300

    Setup basic pdf converter by ghostscript

---
+++ pdfcompress.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+iconpath=$(dirname "$0")"/icon-pdf-128.png"
+description="Compress PDF document by ghostscript
+
+Quality:
+⦁ screen (screen-view-only quality, 72 dpi images)
+⦁ ebook (low quality, 150 dpi images)
+⦁ printer (high quality, 300 dpi images)
+⦁ prepress (high quality, color preserving, 300 dpi imgs)
+"
+
+fields=$(yad --form --title="Compress PDF document" \
+    --window-icon="$iconpath" \
+    --image="$iconpath" \
+    --width=600 \
+    --text="$description" \
+    --item-separator=";" \
+    --field="Compression":CB 'screen;ebook;^printer;prepress' \
+    --field="Document":FL \
+    --field="Output directory":DIR \
+    --field="Filename" \
+    --button="Compress")
+    
+echo $fields
+IFS='|' read -a arguments <<< "${fields}"
+
+gs -sDEVICE=pdfwrite -dNOPAUSE -dBATCH "-dPDFSETTINGS=/${arguments[0]}" "-sOutputFile=${arguments[2]}/${arguments[3]}" "${arguments[1]}" && echo "Converted ${arguments[1]} to ${arguments[2]}/${arguments[3]}"
